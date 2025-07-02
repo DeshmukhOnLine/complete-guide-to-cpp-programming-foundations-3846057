ff720a9acb92bc109a98f6f0f62d57646c6843d2
Ani Deshmukh <aniruddha.deshmukh@cytel.com>
2025-07-02 11:31:00 +0530

    my first C++ program after a long time!

---
+++ src/Ch00/MyHelloWorld.cpp
@@ -0,0 +1,10 @@
+// File: MyHelloWorld.cpp
+// Description: My first C++ program after many, many years!
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!" << std::flush;
+    return 0;
+}
